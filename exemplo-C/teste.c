a78d7bbe926f9a462738dfa7bad4ec0540bfb696
matheuspaz11 <matheuspazmpl@gmail.com>
2020-11-19 17:38:13 -0300

    att no arquivo teste.c

--- exemplo-C/teste.c
+++ exemplo-C/teste.c
@@ -5,5 +5,9 @@ int main(){
 
     printf("Ola professor!");
     printf("Exemplo de versionamento com arquivo em C!");
+    int i, j; 
+    i = 5;
+    j = 2;
+    printf("%d", i+j);
     return 0;
 }
